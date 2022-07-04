b48d3189a9244006d2def1ad72225d168a7f72a4
Elvis <Elvisraphael00@gmail.com>
2022-07-04 03:09:48 -0700

    upadte to 2,3,4,5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,17 @@
 #include <string.h>
 
 /**
- *main - Main function of my project
+*main - Main function of my project
 *
 *
 *Return: return 0 everytime
 */
-int main(void){
-	char displayed [52];
+int main(void)
+{
 
-	strcpy(displayed, "\"Programming is like building a multilingual puzzel");
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
 	puts(displayed);
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main(void){
+/**
+*main - Main function
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
